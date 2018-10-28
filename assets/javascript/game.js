91dc731d0422f93d5d7f41fe7072022be2427cad
z1995 <zhannaavanesova@yahoo.com>
2018-10-28 17:50:57 -0600

    Latest Changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,76 @@
+// var randomNumber = Math.floor((Math.random() * 300));
+// var crystalOne= Math.floor((Math.random() * 30));
+// var crystalTwo = Math.floor((Math.random() * 30));
+// var crystalThree = Math.floor((Math.random() * 30));
+// var crystalFour = Math.floor((Math.random() * 30));
+// var userScore = 0;
+// var wins = 0;
+// var losses = 0;
+
+// function reset() {
+//     randomNumber = Math.floor((Math.random() * 300));
+//     crystalOne;
+//     crystalTwo;
+//     crystalThree;
+//     crystalFour;
+//     userScore = 0;
+// };
+
+// function win() {
+//     alert("Congratulations!!! YOU WON!");
+//     wins++;
+//     $("#wins").text("Wins: " + wins);
+// };
+
+// function lose() {  
+//     alert("Better luck next time! Try Again!"); 
+//     losses++;
+//     $("#losses").text("Losses: " + losses);
+// };
+    
+// $(document).ready(function() {
+//     $("#start").on("click", function startGame() {
+//         if(userScore === randomNumber) {
+//             win();
+//             reset();
+//         }
+//         else if(userScore > randomNumber) {
+//             lose();
+//             reset();
+//         };
+        
+//         $("#random-number").text(randomNumber);
+//         $("#your-score").text("Your Score: " + userScore);
+//         $("#wins").text("Wins: " + wins);
+//         $("#losses").text("Losses: " + losses);
+
+//         $("#crystal-1").on("click", function() {
+//         userScore += crystalOne;
+//         $("#your-score").text("Your Score: " + userScore);
+//         winLose();
+//         });
+
+//         $("#crystal-2").on("click", function() {
+//         userScore += crystalTwo;
+//         $("#your-score").text("Your Score: " + userScore);       
+//         winLose();     
+//         });
+    
+//         $("#crystal-3").on("click", function() {
+//         userScore += crystalThree;
+//         $("#your-score").text("Your Score: " + userScore);
+//         winLose();
+//         });
+    
+//         $("#crystal-4").on("click", function() {
+//         userScore += crystalFour;
+//         $("#your-score").text("Your Score: " + userScore); 
+//         winLose();   
+//         });
+//     });
+    
+// });
+
 var randomNumber = Math.floor((Math.random() * 300));
 var crystalOne= Math.floor((Math.random() * 30));
 var crystalTwo = Math.floor((Math.random() * 30));
@@ -8,20 +81,23 @@ var wins = 0;
 var losses = 0;
 
 function reset() {
-    randomNumber;
+    randomNumber = Math.floor((Math.random() * 300));
+    $("#random-number").text(randomNumber);
     crystalOne;
     crystalTwo;
     crystalThree;
     crystalFour;
     userScore = 0;
 };
-function winLose() {
-    if (userScore === randomNumber) {
+function win() {
+    if (userScore == randomNumber) {
     $("#wins").text("Wins: " + wins);
     alert("Congratulations!!! YOU WON!");
     wins++;
     reset();
-};
+}};
+
+function lose() {
 if (userScore > randomNumber) {
     losses++;
     $("#losses").text("Losses: " + losses);
@@ -39,28 +115,36 @@ $(document).ready(function() {
         $("#crystal-1").on("click", function() {
         userScore += crystalOne;
         $("#your-score").text("Your Score: " + userScore);
-        winLose();
+        win();
+        lose(); 
         });
     
         $("#crystal-2").on("click", function() {
         userScore += crystalTwo;
         $("#your-score").text("Your Score: " + userScore);       
-        winLose();     
+        win();
+        lose();     
         });
     
         $("#crystal-3").on("click", function() {
         userScore += crystalThree;
         $("#your-score").text("Your Score: " + userScore);
-        winLose();
+        win();
+        lose();
         });
     
         $("#crystal-4").on("click", function() {
         userScore += crystalFour;
         $("#your-score").text("Your Score: " + userScore); 
-        winLose();   
+        win();   
+        lose();
         });
+
+        console.log(randomNumber);
+        console.log(reset);
 
     });
     
 });
+
 
